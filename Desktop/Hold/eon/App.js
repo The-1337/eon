f3a388e3930da1a8ac3dd1f1477b0c77012b3b2b
joshuapawlik <joshawesome12@yahoo.com>
2018-01-24 15:38:32 -0600

    migration to react native

---
+++ Desktop/Hold/eon/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import Footer from './footer'
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://pre00.deviantart.net/9aaf/th/pre/f/2007/174/4/f/katana_by_eon_art.jpg'
+    };
+    return (
+
+      <View style={{flex: 1}}>
+        <View style={styles.mainContent}>
+          <View style={{flex: 1, backgroundColor: 'black'}} />
+          <View style={{flex: 1, backgroundColor: 'rgb(32,190,84)'}} />
+          <View style={{flex: 1, backgroundColor: 'black'}} />
+        </View>
+        <Footer style={styles.footer}/>
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContent: {
+    flex: 9,
+    backgroundColor: 'black',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  footer:{
+    flex:2
+  }
+});
